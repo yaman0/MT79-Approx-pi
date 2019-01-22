6827f58c6de25edbf83f6f879a8bd67a8f8f0807
yaman0 <yaman0@protonmail.com>
2019-01-22 10:01:38 +0100

    ref(PiGenerator): refactor code

--- src/PiGenerator.py
+++ src/PiGenerator.py
@@ -5,23 +5,23 @@ class PiGenerator:
     def __init__(self):
         self.value = 0
 
-    def SerieInvCarres(self,n):
+    def SerieInvCarres(self, n):
         """
         Return the "serieinvcarres" with n depth
         :param int n: max of sum
         :return: serieinvcarres
         """
-        if(n<0):
+        if (n < 0):
             raise ValueError('N need to be positive')
         result = 0
-        for k in range(1, n+1):
+        for k in range(1, n + 1):
             result += 1 / math.pow(k, 2)
         return result
 
-    def MethodeSerieInvCarres(self,n):
+    def MethodeSerieInvCarres(self, n):
         """
         Compute approach of pi with SerieInvCarres
         :param int n: depth of SerieInvCarres
         :return float: approach of pi
         """
-        return math.sqrt(6*self.SerieInvCarres(n))
+        return math.sqrt(6 * self.SerieInvCarres(n))
