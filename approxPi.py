e3e5bb950fc6212ebf84e68b95c926fc803da3d0
yaman0 <yaman0@protonmail.com>
2019-01-23 23:19:56 +0100

    Merge branch 'master' of /home/hyds/Documents/Codes/MT79-Approx-pi with conflicts.

--- approxPi.py
+++ approxPi.py
@@ -4,46 +4,21 @@ from src.Data import Data
 from src.Graph import Graph
 from src.PiGenerator import PiGenerator
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--method", help="choose the method (normal(n), imparis(i))",
+                    type=str, default='n')
+parser.add_argument("depth", help="depth of sum",
+                    type=int)
+args = parser.parse_args()
+engine = PiGenerator()
+# if args.method == 'n':
+#     print(engine.MethodeSerieInvCarres(args.depth))
+# elif args.method == 'i':
+#     print(engine.MethodeSerieInvCarresImparis(args.depth))
+c = []
+i = []
+for k in range(0, args.depth + 1):
+    c.append(engine.MethodeSerieInvCarres(k) - engine.realPi())
+    i.append(engine.MethodeSerieInvCarresImparis(k) - engine.realPi())
 
-def gen_pi(args):
-    engine = PiGenerator()
-    if args.method == 'n':
-        print(engine.MethodeSerieInvCarres(args.depth))
-    elif args.method == 'i':
-        print(engine.MethodeSerieInvCarresImparis(args.depth))
-
-
-def displayGraphDiffMethods(args):
-    engine = PiGenerator()
-    view = Graph()
-    result_normal = []
-    result_imparis = []
-    for k in range(0, args.depth):
-        result_normal.append(engine.MethodeSerieInvCarres(k) - engine.realPi())
-        result_imparis.append(engine.MethodeSerieInvCarresImparis(k) - engine.realPi())
-    view.addData(Data(result_normal, label='classic')) \
-        .addData(Data(result_imparis, label='imparis')) \
-        .showLegend() \
-        .view()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    # Generation of pi parse
-    subparser = parser.add_subparsers()
-    genpi_pars = subparser.add_parser('genpi', help='Generate pi')
-    genpi_pars.add_argument("--method", help="Choose the method (normal(n), imparis(i))",
-                            type=str, default='n')
-    genpi_pars.add_argument("depth", help="Depth of sum",
-                            type=int)
-    genpi_pars.set_defaults(func=gen_pi)
-
-    # Generation of diffgraph parse
-    diffgraph_pars = subparser.add_parser('diffgraph', help='Generate pi')
-    diffgraph_pars.add_argument("depth", help="Depth of sum",
-                                type=int)
-    diffgraph_pars.set_defaults(func=displayGraphDiffMethods)
-
-    args = parser.parse_args()
-    args.func(args)
+Graph().showLegend().addData(Data(c, label='classic')).addData(Data(i, label='imparis')).view()
